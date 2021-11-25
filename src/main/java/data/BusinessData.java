5cd1c014f0d9ae49e5e2e3d4cb5daae9bc997fa3
Cigdem <cigdemrespect@gmail.com>
2021-11-25 11:47:11 -0600

    Business Data

--- src/main/java/data/BusinessData.java
+++ src/main/java/data/BusinessData.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class BusinessData {
 
     public static List<String> getExpectedAppNames(){
-        return Arrays.asList("Find Patient Record","Active Visits","Register a patient",
-                "Capture Vitals","Appointment Scheduling","Reports","Data Management",
-                "Configure Metadata","System Administration");
+        return Arrays.asList("Find Patient Record","Active Visits","Register a patient","Capture Vitals",
+                "Appointment Scheduling","Reports","Data Management","Configure Metadata","System Administration");
     }
 }
