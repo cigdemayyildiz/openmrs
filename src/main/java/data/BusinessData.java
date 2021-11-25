8f5f7e73a99d1d0bf092f37fafde1a95d47e1f8e
Cigdem <cigdemrespect@gmail.com>
2021-11-25 10:01:07 -0600

    Business Data

--- src/main/java/data/BusinessData.java
+++ src/main/java/data/BusinessData.java
@@ -9,9 +9,4 @@ public class BusinessData {
                 "Capture Vitals","Appointment Scheduling","Reports","Data Management",
                 "Configure Metadata","System Administration");
     }
-
-    public static List<String> getExpectedPatientData(){
-        return Arrays.asList("Name: David, Hunt","Gender: Male","Birthdate: 16, March, 1997",
-                "Address: 2200 Devon","Phone Number: 2456667788","Relatives: John White - Doctor");
-    }
 }
